d75aa707d03ff0d7f3f6c8908460d06b12df9089
EmilioOcelotl <emilio.ocelotl@gmail.com>
2022-12-09 13:33:06 -0600

    hydra y mapas

--- index.js
+++ index.js
@@ -14,8 +14,23 @@ import { FontLoader } from './jsm/loaders/FontLoader.js';
 import { Flow } from './jsm/modifiers/CurveModifier.js';
 import { TextGeometry } from './jsm/geometries/TextGeometry.js';
 
+// hydra
+
+let switchhydra = 0;
+
+var hydra = new Hydra({
+    canvas: document.getElementById("myCanvas"),
+    detectAudio: false,
+    //makeGlobal: false
+}) // antes tenía .synth aqui 
+
+const elCanvas = document.getElementById( 'myCanvas');
+elCanvas.style.display = 'none';     
+let vit = new THREE.CanvasTexture(elCanvas);
+
 // RenderTarget 
 
+let cubort; 
 const rtWidth = 1920*2;
 const rtHeight = 1080;
 const renderTarget = new THREE.WebGLRenderTarget(rtWidth, rtHeight, { format: THREE.RGBAFormat } );
@@ -27,7 +42,6 @@ const rtCamera = new THREE.PerspectiveCamera(rtFov, rtAspect, rtNear, rtFar);
 rtCamera.position.z = 4;
 
 const rtScene = new THREE.Scene();
-
 
 let moveForward = false;
 let moveBackward = false;
@@ -97,8 +111,6 @@ loadertext.load(
 	    }
 	}
 );
-
-console.log(lineasInicio); 
  
 function init(){
     
@@ -115,8 +127,9 @@ function init(){
     
     camera.position.set(0, 0, 40 );
 
-    const path = 'img/';
-    const format = '.jpg';
+    /*
+    const path = 'maps/1-cueva/';
+    const format = '.png';
     const urls = [
 	path + 'px' + format, path + 'nx' + format,
 	path + 'py' + format, path + 'ny' + format,
@@ -127,28 +140,41 @@ function init(){
     const refractionCube = new THREE.CubeTextureLoader().load( urls );
     refractionCube.mapping = THREE.CubeRefractionMapping;
     scene.background = reflectionCube;
+    */
 
+    fondos(); 
+    
     light = new THREE.PointLight( 0xffffff, 1 );
     light.position.set( 0, 20, 10 );
     scene.add( light ); 
+
     //light = new THREE.DirectionalLight( 0xffaa33 );
     //light.position.set( - 10, 10, 10 );
     //light.intensity = 1.0;
     //scene.add( light )
     
-    //const light2 = new THREE.AmbientLight( 0x003973 );
-    //light2.intensity = 1.0;
-    //scene.add( light2 );
+    const light2 = new THREE.AmbientLight( 0x003973 );
+    light2.intensity = 1.0;
+    scene.add( light2 );
+
+    const geometryTor = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+    const materialTor = new THREE.MeshBasicMaterial( { color: 0xffffff, map: vit } );
+    const torusKnot = new THREE.Mesh( geometryTor, materialTor );
+    scene.add( torusKnot );
+
+    swhydra(0);
 
     const materialrt = new THREE.MeshBasicMaterial({
 	map: renderTarget.texture,
 	transparent: true
     });
 
-    const geometry = new THREE.BoxGeometry( 1920, 1080/2, 1 );
+    //const geometry = new THREE.BoxGeometry( 1920, 1080/2, 1 );
+    const geometry = new THREE.PlaneGeometry( 1920, 1080/2, 20, 20 );
+
     const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    const cube = new THREE.Mesh( geometry, materialrt );
-    scene.add( cube );
+    cubort = new THREE.Mesh( geometry, materialrt );
+    scene.add( cubort );
     
     renderer = new THREE.WebGLRenderer( { antialias: true, alpha:true } );
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -314,11 +340,12 @@ function onWindowResize() {
 
 function animate(){
     render();
-    
     requestAnimationFrame ( animate );
 }
 
 function render() {
+    
+    vit.needsUpdate = true; 
 
     controles(); 
 
@@ -326,16 +353,12 @@ function render() {
     case 0:
 	// inicio
 	break; 
-    case 0:
-	// No hay actualización
-	break;
     case 1:
-	// Ya hay actualización
+	break; 
     case 2:
 	up2();
 	break; 
     }
-
     
     renderer.setRenderTarget(renderTarget);
     
@@ -382,7 +405,9 @@ function rm1(){
 // segunda escena si necesita actualización 
 
 function add2(){
-    
+
+    console.log("segunda");
+    escena = 2; 
     let numero = Math.floor(Math.random()*lineasInicio.length); 
     lineasSelectas.unshift( lineasInicio[numero]+"\n");
     if( lineasSelectas.length > 12){
@@ -396,19 +421,19 @@ function add2(){
 
 function up2(){
     if(booltext){
-	const time = Date.now() * 0.0001;
+	const time = Date.now() * 0.001;
 	let perlin = new ImprovedNoise();
-	for( var i = 0; i < text.geometry.attributes.position.count; i++){
-	    let d = perlin.noise(text.geometry.attributes.position.getX(i)+time,
-				 text.geometry.attributes.position.getY(i)+time,
-				 text.geometry.attributes.position.getZ(i)+time ) * 0.1
-	    text.geometry.attributes.position.setX(i, clonX[i] * (d+1));
-	    text.geometry.attributes.position.setY(i, clonY[i] * (d+1));
-	    text.geometry.attributes.position.setZ(i, clonZ[i] * (d+1));
+	for( var i = 0; i < cubort.geometry.attributes.position.count; i++){
+	    let d = perlin.noise(cubort.geometry.attributes.position.getX(i)+time,
+				 cubort.geometry.attributes.position.getY(i)+time,
+				 cubort.geometry.attributes.position.getZ(i)+time ) * 0.5
+	    cubort.geometry.attributes.position.setX(i, clonX[i] * (d+1));
+	    cubort.geometry.attributes.position.setY(i, clonY[i] * (d+1));
+	    cubort.geometry.attributes.position.setZ(i, clonZ[i] * (d+1));
 
     }	
-	text.geometry.attributes.position.needsUpdate = true;
-	text.geometry.computeVertexNormals();
+	cubort.geometry.attributes.position.needsUpdate = true;
+	cubort.geometry.computeVertexNormals();
     }
 }
 
@@ -440,25 +465,28 @@ function texto( mensaje ){
 }
 
 function texto2( mensaje ){
-    //const materialT = new THREE.MeshStandardMaterial({color: 0xffffff, metalnenss: 0.8, roughness: 0.2, flatShading: true});
+
+        //const materialT = new THREE.MeshStandardMaterial({color: 0xffffff, metalnenss: 0.8, roughness: 0.2, flatShading: true});
 
     const materialT = new THREE.MeshBasicMaterial({color: 0xffffff});
     text.material = materialT; 
-    const shapes = font.generateShapes( mensaje, 2 );
+    const shapes = font.generateShapes( mensaje, 0.2 );
     const geometry = new THREE.ShapeGeometry( shapes );
-    textGeoClon = geometry.clone(); // para modificar
+    // textGeoClon = geometry.clone(); // para modificar
     text.geometry.dispose(); 
     text.geometry= geometry;
     geometry.computeBoundingBox();
     geometry.computeVertexNormals(); 
     // const xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
-    geometry.translate( 0,40, 0 );
+    //geometry.translate( xMid, 0, 0 );
     text.geometry= geometry;
-    scene.add(text); 
-    text.position.y = 10;
+    rtScene.add(text); 
+    text.position.y = 2.5;
+    text.position.x = -10.5; 
+
     booltext = true;
 
-    textClone = text.clone(); 
+    textClone = cubort.clone(); 
     
     for(let i = 0; i < textClone.geometry.attributes.position.count; i++){
 	clonX[i] = textClone.geometry.attributes.position.getX(i);
@@ -499,4 +527,332 @@ function controles(){
     }
         prevTime = time;
 
+}
+
+function swhydra( ) {
+
+    let rand = Math.floor(Math.random() * 10);
+    
+    switch( rand ){
+    case 0:
+    src(o1).layer(
+	src(o1).mask(
+	    noise(2,.01).sub(noise(2,.01).modulate(solid(1,0),.003)).r().thresh(0.01,0))
+	    .colorama(.01))
+	.modulate(
+	    osc(Math.PI*2,0,Math.PI/2).scale(1/2).brightness(-.5).modulate(
+		noise(2,.01).sub(gradient())
+		,1),.01)
+	.modulatePixelate(
+	    osc(Math.PI*2,0,Math.PI/2).r().thresh(.15,0).color(1,0,0).add(
+		osc(Math.PI*2,0,Math.PI).g().thresh(.15,0).color(0,1,0))
+		.scale(.25)
+		.modulate(noise(2,.01).sub(gradient()),1),10400,128)
+	.layer(
+	    osc(15,0.1,1.5)
+		.mask(
+		    shape(4,.2,0))
+	)
+	.out(o1)
+    
+    solid().layer(o1).out()
+    break;
+    
+    case 1:
+	osc(5, 0.15, 9)
+	    .rotate(.5)
+	    .mult(osc(25, .05, 9))
+	    .modulate(voronoi()
+		      .modulateScale(voronoi()
+				     .brightness(0.5)
+				     .contrast(1.95), 0.1)
+		      .modulate(voronoi(1.5, .1)
+				.brightness(0.15)
+				.contrast(1.95)
+              			, .5), 1.)
+	    .sub(gradient())
+	    .modulateHue(src(o0), 500)
+	    .out()
+	break;
+	
+    case 2:
+	osc(22, 0.3, 2)
+	    .modulateScale(osc()
+			   .rotate(Math.PI / 2))
+	    .rotate(0.1, 1)
+	    .modulate(noise(), [0.1, 0.5, 0.6, 1, 0.2])
+	    .out(o1)
+	
+	src(o0)
+	    .layer(src(o1)
+		   .mask(shape(2)
+			 .scale(4, 0.001)
+			 .scrollX(0.5)))
+	    .scrollX(0.00075)
+	    .out() 
+
+	break;
+	
+    case 3:
+	src(o0)
+	    .modulate(
+		gradient()
+		    .pixelate(2,2)
+		    .brightness(-0.5)
+		    .mult(o0,1)
+		    .color([0,0,1/128,-1/128,1/64,-1/64,3/32,-1/32].fast(1/2).smooth(),[0,0,1/128,-1/128,1/64,-1/64,1/32,-1/32].fast(1/2.1).smooth())
+		, -0.75
+	    )
+	    .layer(
+		osc(Math.PI*16,1/48,Math.PI/1.5)
+		    .rotate(Math.PI/4,1/128)
+		    .mask(
+			shape(99,[1/256,1/32].fast(1/32).smooth(),0)
+		    )
+	    )
+	    .modulate(o0,-1/512)
+	    .hue(0.001)
+	    .out()
+	break;
+    case 4:
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+	var a = 900;
+	var b = 1600;
+	let w = width / b;
+	let h = height / a;
+	
+	let delta = (l, m) => gradient(l)
+	    .diff(solid("st.x", "st.y", () => Math.sin(time * m)))
+	
+	let synth = (i, j, k, l, m) => osc(i, j, k)
+	    .diff(delta(l, m))
+	
+	function form(f, g, i, j, k, l, m, n) {
+	    if (n % 2 === 1) {
+   		return shape(n, "st.y+st.y", f)
+   		    .mult(synth(i, j, k, l, m)
+   			  .kaleid(n)
+   			  .rotate(Math.PI / 2))
+   		 .scale(1 / 2 + f)
+   		    .rotate(1 / 10, 1 / 10)
+   		    .scale(g);
+	    } else if (n % 2 === 0) {
+   		if (n % 4 === 0) {
+   		    return shape(n, "st.y+st.y", f)
+   			.mult(synth(i, j, k, l, m)
+   			      .kaleid(n)
+   			      .rotate(Math.PI / n))
+   			.scale(1 / 2 + f)
+   			.rotate(1 / 10, 1 / 10)
+   			.scale(g);
+   		} else {
+   		    return shape(n, "st.y+st.y", f)
+   			.mult(synth(i, j, k, l, m)
+   			      .kaleid(n)
+   			      .rotate(Math.PI / (n / 2)))
+   			.scale(1 / 2 + f)
+   			 .rotate(1 / 10, 1 / 10)
+   			.scale(g);
+   		}
+	    }
+	}
+	
+	function axis(f, g, h, i, j, k, l, m, n, o, v, w) {
+	    if (v === "x0") {
+   		return form(f, g, i, j, k, l, m, n)
+   		    .scrollX(() => (time / o))
+   		    .scale("st.x+st.x")
+   		    .scale(h, width / height, w);
+	    } else if (v === "x1") {
+   		return form(f, g, i, j, k, l, m, n)
+   		    .scrollX(() => (time / o))
+   		 .scale("st.x*st.x")
+   		    .scale(h, width / height, w);
+	    } else if (v === "y0") {
+   		return form(f, g, i, j, k, l, m, n)
+   		    .scrollY(() => (time / o))
+   		    .scale("st.y+st.y")
+   		    .scale(h, width / height, w);
+	    } else if (v === "y1") {
+   	 return form(f, g, i, j, k, l, m, n)
+   		 .scrollY(() => (time / o))
+   		 .scale("st.y*st.y")
+   		 .scale(h, width / height, w);
+    } else if (v === "z0") {
+   	 return form(f, g, i, j, k, l, m, n)
+   		 .scroll(() => (time / o), () => (time / o))
+   		 .scale("st.x+st.y")
+   		 .scale(h, width / height, w);
+    } else if (v === "z1") {
+   	 return form(f, g, i, j, k, l, m, n)
+   		 .scroll(() => (time / o), () => (time / o))
+   		 .scale("st.x*st.y")
+   		 .scale(h, width / height, w);
+    };
+};
+
+	axis(1 / 4, 1 / 2, a, 15, 1 / 16, 300, 1 / 2, 1, 4, 10, "y1", b)
+	    .scale(1 / 50)
+	    .diff(gradient(2)
+   		  .luma())
+	    .modulateScale(osc(Math.PI + Math.E))
+	    .blend(o0, 9 / 10)
+	    .out()
+
+	break;
+    case 5:
+	let k=()=>100
+	let d=(x)=>Math.PI*x/k()
+	
+	src(o1).hue(.01)
+	    .modulate(
+		osc(30,0,1.5).brightness(-.5).modulate(noise().sub(gradient())
+						       ,1),.003)
+	    .layer(
+		osc(5,0,1.5)
+	  .mask(
+	      shape(9,()=>d(.5),0)).scrollY(()=>d(.5)/2).rotate(()=>Math.atan2(d(.5),1))
+		    .scrollY(()=>-d(.5)/2-.5)
+		    .kaleid(()=>k()*.25).scale(.25)
+		    .mask(shape(999,.5).rotate(Math.PI/8))
+		    .scale("st.y+st.x")
+		    .modulate(osc(30,0,1.5).brightness(-.5).modulate(noise().sub(gradient()),1),.01)
+)
+	    .out(o1)
+
+
+solid().layer(o1).out()
+
+	break;
+    case 6:
+	src(o0)
+    .saturate(9)
+    .brightness([-1, 1].smooth(1 / 9)
+   	 .fast(1 / 2))
+    .contrast([0, 3].smooth(1 / 3)
+   	 .fast(1 / 9))
+    .modulateHue(src(o0)
+   	 .scale("st.x+st.x")
+   	 .scale("st.y+st.y")
+   	 .scale(1.01), () => Math.tan(time / 100))
+    .layer(osc(1, 2, 9)
+   	   .sub(gradient(3)
+   		.mask(src(o0)
+   		      .diff(src(o0)
+   			    .pixelate(5, 1)
+   			    .mult(shape(9, 0, 1)
+   				  .repeat(5, 2)
+   				  .invert())
+   			    .diff(src(o0)
+   				  .scale("st.x+st.y")
+   				  .diff(src(o0)
+   					.scale(1.01))))
+   		      .mask(shape(4, 1 /2, 1 / 2))
+   		      .colorama(9), 3 / 10)))
+	    .out()
+	
+	break;
+    case 7:
+	speed = 1;
+	shape(20, 0.2, 0.3)
+	    .color(0.5, 1.4,100)
+	    .scale(1.01)
+	    .repeat(9,9)
+	    .modulateScale(osc(5,1.5).modulate(o0,2))
+	    .scale(0.99)
+	    .modulate(noise(4, 4))
+	    .rotate(1, .2)
+	    .out(o0)
+	break;
+    case 8:
+	let k1=()=>100
+	let d1=(x)=>Math.PI*x/k1()
+	
+	src(o1).colorama(.001)
+	    .modulate(
+		osc(6,0,1.5).brightness(-.5).modulate(noise().sub(gradient())
+						      .add(
+							  solid(1,1,1).mask(
+							      shape(2,()=>d1(1),0.1)).scrollY(()=>d1(2)/2).rotate(()=>Math.atan2(d1(2),1))
+							      .scrollY(()=>-d1(2)/2-.5)
+							      .kaleid(()=>k1()*.25).scale(.25)
+							      .mask(shape(999,.5).rotate(Math.PI/8))
+							      .modulateRotate(shape(999,0,1),1).scale(2),-1
+						      ),3.14),.003)
+	    .layer(
+		osc(5,0,1.5)
+		    .modulateScale(osc(1,0),-.2,1)
+		    .pixelate(5)
+		    .scrollX(.3)
+		    .scale(1,1.25)
+		    .mask(
+			shape(2,()=>d1(.5),0)).scrollY(()=>d1(.5)/2).rotate(()=>Math.atan2(d1(.5),1))
+		    .scrollY(()=>-d1(.5)/2-.5)
+		    .kaleid(()=>k1()*.25).scale(.25)
+		    .mask(shape(999,.5).rotate(Math.PI/8))
+		    .modulateRotate(shape(999,0,1),1).scale(2)
+	    )
+	    .out(o1)
+	
+	solid().layer(o1).out()
+
+	break;
+    case 9:
+	let k2=()=>100
+	let d2=(x)=>Math.PI*x/k2()
+
+	src(o1).colorama(.001)
+	    .modulate(
+		osc(6,0,1.5).brightness(-.5).modulate(noise().sub(gradient())
+						      .add(
+							  solid(1,1,1).mask(
+							      shape(2,()=>d2(1),0.1)).scrollY(()=>d2(2)/2).rotate(()=>Math.atan2(d2(2),1))
+							      .scrollY(()=>-d2(2)/2-.5)
+							      .kaleid(()=>k2()*.25).scale(.25)
+							      .mask(shape(999,.5).rotate(Math.PI/8))
+	.modulateRotate(shape(999,0,1),1).scale(2),-1
+  ),3.14),.003)
+  .layer(
+  osc(5,0,1.5)
+  .modulateScale(osc(1,0),-.2,1)
+  .pixelate(5)
+  .scrollX(.3)
+  .scale(1,1.25)
+  .mask(
+  shape(2,()=>d2(.5),0)).scrollY(()=>d2(.5)/2).rotate(()=>Math.atan2(d2(.5),1))
+  .scrollY(()=>-d2(.5)/2-.5)
+  .kaleid(()=>k2()*.25).scale(.25)
+  .mask(shape(999,.5).rotate(Math.PI/8))
+  .modulateRotate(shape(999,0,1),1).scale(2)
+)
+  .out(o1)
+
+solid().layer(o1).out()
+	break; 
+    }
+}
+
+
+function fondos( ) {
+
+    const patharray = ['1-cueva', '2-piedras', '3-sol', '4-verde', '5-espacio', '6-pasto', '7-tronco', '8-lago']; 
+
+    let rand = Math.floor(Math.random()*7); 
+
+    
+    
+    const path = 'maps/' + patharray[rand]+'/';
+    const format = '.png';
+    const urls = [
+	path + 'px' + format, path + 'nx' + format,
+	path + 'py' + format, path + 'ny' + format,
+	path + 'pz' + format, path + 'nz' + format
+    ];
+
+    const reflectionCube = new THREE.CubeTextureLoader().load( urls );
+    const refractionCube = new THREE.CubeTextureLoader().load( urls );
+    refractionCube.mapping = THREE.CubeRefractionMapping;
+    scene.background = reflectionCube;
+    
 }
